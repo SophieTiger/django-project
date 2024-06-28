8ff8eec092af876bbf27adec7aa343360e35918c
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-06-28 12:59:21 +0000

    Add procfile and modify settings.py for deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-21ozpzy(v(bxjg!z-e*zgo_5qg&7db0-d*qn5g8f!w_g)gmvj_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-sophietiger-djangoproje-gczik5kvaex.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-sophietiger-djangoproje-gczik5kvaex.ws.codeinstitute-ide.net',
+'.herokuapp.com']
 
 
 # Application definition
